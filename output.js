5390499b04e6adffb39148d51dceb3a628a27651
azu <azuciao@gmail.com>
2015-07-15 23:16:35 +0900

    update POC

--- output.js
+++ output.js
@@ -1,2 +1,47 @@
 // LICENSE : MIT
 "use strict";
+var createFormatter = require('power-assert-formatter');
+var formatter = createFormatter();
+// ? https://github.com/power-assert-js/empower/blob/c788718db868e3a5927cf166d08b2ef9336f9d0e/lib/decorate.js
+var result = {
+    "powerAssertContext": {
+        "value": false,
+        "events": [
+            {
+                "value": [
+                    1,
+                    2,
+                    3
+                ],
+                "espath": "arguments/0/left/callee/object"
+            },
+            {
+                "value": 2,
+                "espath": "arguments/0/left/arguments/0"
+            },
+            {
+                "value": 1,
+                "espath": "arguments/0/left"
+            },
+            {
+                "value": -1,
+                "espath": "arguments/0/right"
+            },
+            {
+                "value": false,
+                "espath": "arguments/0"
+            }
+        ]
+    },
+    "source": {
+        "content": "t.ok(ary.indexOf(two) === minusOne, 'THIS IS AN ASSERTION MESSAGE')",
+        "filepath": "test.js",
+        "line": 44
+    }
+};
+var context = {
+    source: result.source,
+    args: [result.powerAssertContext]
+};
+var text = formatter(context);
+console.log(text);
--- test.js
+++ test.js
@@ -1,2 +1,56 @@
 // LICENSE : MIT
 "use strict";
+var test = require('tape');
+var cap = capturable();
+test.Test.prototype._capt = cap._capt;
+test.Test.prototype._expr = cap._expr;
+var path = require('path');
+test.createStream({objectMode: true}).on('data', function (row) {
+    console.log(JSON.stringify(row, null, 4))
+});
+process.argv.slice(2).forEach(function (file) {
+    require(path.resolve(file));
+});
+function capturable() {
+    var events = [];
+
+    function _capt(value, espath) {
+        events.push({value: value, espath: espath});
+        return value;
+    }
+
+    function _expr(value, args) {
+        var captured = events;
+        events = [];
+        return {
+            powerAssertContext: {
+                value: value,
+                events: captured
+            },
+            source: {
+                content: args.content,
+                filepath: args.filepath,
+                line: args.line
+            }
+        };
+    }
+
+    return {
+        _capt: _capt,
+        _expr: _expr
+    };
+}
+test('timing test', function (t) {
+    t.plan(2);
+
+    t.equal(typeof Date.now, 'function');
+
+    var ary = [1, 2, 3];
+    var minusOne = -1, two = 2;
+    t.ok(ary.indexOf(two) === minusOne, 'THIS IS AN ASSERTION MESSAGE');
+
+    var start = Date.now();
+    setTimeout(function () {
+        t.equal(Date.now() - start, 100);
+    }, 100);
+});
